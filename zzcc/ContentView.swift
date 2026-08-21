5f99a9aa14e39566ea39d04d827afedef8a4344b
zz <zzr1285690197@gmail.com>
2026-08-21 15:24:36 +0800

    Initial Commit

---
+++ zzcc/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  zzcc
+//
+//  Created by 朱梓榕 on 2026/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ zzcc/zzccApp.swift
@@ -0,0 +1,17 @@
+//
+//  zzccApp.swift
+//  zzcc
+//
+//  Created by 朱梓榕 on 2026/8/21.
+//
+
+import SwiftUI
+
+@main
+struct zzccApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
